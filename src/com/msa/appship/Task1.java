384d56a0669570044c3538917b269687bdfbaced
mx <iraqrev@!gmail.com>
2017-12-15 17:47:06 +0200

    First commit 1

--- src/com/msa/appship/Task1.java
+++ src/com/msa/appship/Task1.java
@@ -1,6 +1,6 @@
 package com.msa.appship;
 
-public class Main {
+public class Task1 {
 
 //    If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 //
